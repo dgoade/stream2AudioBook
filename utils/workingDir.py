36a09262c583069b20484b6fb2652fc185e03af6
dgoade <dgoade@gmail.com>
2014-12-29 08:45:18 -0500

    Correcting content lost in the rename

--- utils/workingDir.py
+++ utils/workingDir.py
@@ -4,48 +4,51 @@ import sys
 import os.path
 import logging
 
-class Working_dir():
+class workingDir():
 
-    def __init__(self, dir_name, logging_dict = None ):
+    logger = None
 
-        if logging_dict:
-            logger = logging.getLogger('Working_dir.{0}.__init__'.format(__name__))
-            log_msg = 'Logging config dictionary passed so using logging'
-            logger.debug(log_msg)
+    def __init__(self, logging_dict = None ):
+
+        rval = True
+        logger_name = '{0}.__init__'.format(__name__)
+        logger = logging.getLogger(logger_name)
+
+    def verify_dir(self, **kwargs):
+
+        rval = True
+        logger_name = '{0}.verify_working_dir'.format(__name__)
+        logger = logging.getLogger(logger_name)
+
+        if 'dir_name' in kwargs:
+            dir_name = kwargs['dir_name']
+
+        if 'dir_desc' in kwargs:
+            dir_desc = kwargs['dir_desc']
         else:
-            log_msg = "No logging config dictionary passed to using print"
-            print log_msg
+            dir_desc = "working dir"
 
-#        if dir_name:
-#            log_msg = 'verifying output dir at {0}'.format(test_dir)
-#            logger.debug(log_msg)
-#
-#            if os.path.isdir(test_dir):
-#                log_msg = 'output dir exists at {0}'.format(test_dir)
-#                logger.debug(log_msg)
-#                self.output_dir = test_dir
-#            else:
-#                log_msg1 = 'output dir does not exist at {0}'
-#                log_msg2 = 'so creating it'.format(test_dir)
-#                log_msg = '{0} {1}'.format(log_msg1, log_msg2)
-#                logger.debug(log_msg)
-#                try:
-#                    os.makedirs(test_dir)
-#                    self.output_dir = test_dir
-#                    log_msg = 'output dir created successfully'
-#                    logger.debug(log_msg)
-#                except OSError as exception:
-#                    log_msg1 = 'Exception occurred trying to create'
-#                    log_msg2 = 'output dir: {0}'.format(test_dir)
-#                    log_msg = '{0} {1}'.format(log_msg1, log_msg2)
-#                    logger.exception(log_msg)
-#                    rval = False
-#        else:
-#            log_msg1 = 'No output dir configured. Add an output_dir'
-#            log_msg2 = 'setting to the config file to specify one.'
-#            log_msg = '{0} {1}'.format(log_msg1, log_msg2)
-#            logger.debug(log_msg)
-#            log_msg1 = 'No output dir configured. All output will'
-#            log_msg2 = 'be sent to the current dir.'
-#            log_msg = '{0} {1}'.format(log_msg1, log_msg2)
-#            logger.warn(log_msg)
+        if dir_name:
+            log_msg = 'verifying {0}: {1}'.format(dir_desc, dir_name)
+            logger.debug(log_msg)
+
+            if os.path.isdir(dir_name):
+                log_msg = '{0} exists: {1}'.format(dir_desc, dir_name)
+                logger.debug(log_msg)
+            else:
+                log_msg = 'creating {0}: {1}'.format(dir_desc, dir_name)
+                logger.debug(log_msg)
+                try:
+                    os.makedirs(dir_name)
+                    log_msg = '{0} created: {0}'.format(dir_desc, dir_name)
+                    logger.debug(log_msg)
+                except OSError as exception:
+                    log_msg = ("Exception occurred trying to create"
+                    "{0}: {1}".format(dir_desc, dir_name))
+                    logger.exception(log_msg)
+                    rval = False
+        else:
+            log_msg = 'No {0} passed -- doing nothing.'.format(dir_desc)
+            logger.debug(log_msg)
+
+        return rval
